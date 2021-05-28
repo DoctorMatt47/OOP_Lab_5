2bdd71c55e7a3405fe82a5e5023dd05117213801
DoctorMatt <DoctorMatt47@gmail.com>
2021-05-28 20:09:18 +0300

    Added native find rank algorithm

---
+++ OOP_Lab_5/src/Core/Algorithms/NativeFindRank.cs
@@ -0,0 +1,26 @@
+﻿namespace OOP_Lab_5.Core.Algorithms
+{
+    public class NativeFindRank : IFindRank
+    {
+        public int Execute(Matrix matrix)
+        {
+            var triangular = matrix.Triangular();
+            var rank = 0;
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                var isEnd = true;
+                for (int j = i; j < matrix.Count; j++)
+                {
+                    if (triangular[i, j] != 0)
+                    {
+                        isEnd = false;
+                        rank++;
+                        break;
+                    }
+                }
+                if (isEnd) break;
+            }
+            return rank;
+        }
+    }
+}
