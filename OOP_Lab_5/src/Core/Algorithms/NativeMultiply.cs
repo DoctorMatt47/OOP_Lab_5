73689ceae2c40624d4b4108dbbda795600cf8dc5
DoctorMatt <DoctorMatt47@gmail.com>
2021-05-28 20:51:52 +0300

    Added native multiply algorithm class

---
+++ OOP_Lab_5/src/Core/Algorithms/NativeMultiply.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_5.Core.Algorithms
+{
+    public class NativeMultiply : IMultiply
+    {
+        public Matrix Execute(Matrix left, Matrix right)
+        {
+            if (left.Count != right.Count)
+                throw new ArgumentException();
+
+            Matrix retMatrix = new Matrix(left.Count);
+            for (int i = 0; i < retMatrix.Count; i++)
+            {
+                for (int j = 0; j < retMatrix.Count; j++)
+                {
+                    for (int k = 0; k < retMatrix.Count; k++)
+                    {
+                        retMatrix[i, j] += left[i, k] * right[k, j];
+                    }
+                }
+            }
+            return retMatrix;
+        }
+    }
+}
