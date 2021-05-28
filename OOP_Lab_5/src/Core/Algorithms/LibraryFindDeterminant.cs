1b7eb1b7bf4d45aa56bd4977dadc39a8227c512b
DoctorMatt <DoctorMatt47@gmail.com>
2021-05-28 20:08:21 +0300

    Added implementation of determinant algorithm

---
+++ OOP_Lab_5/src/Core/Algorithms/LibraryFindDeterminant.cs
@@ -0,0 +1,14 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace OOP_Lab_5.Core.Algorithms
+{
+    public class LibraryFindDeterminant : IFindDeterminant
+    {
+        public int Execute(Matrix matrix)
+        {
+            var matrixBuilder = Matrix<double>.Build;
+            var libMatrix = matrixBuilder.Dense(matrix.Count, matrix.Count, (i, j) => matrix[i, j]);
+            return (int)libMatrix.Determinant();
+        }
+    }
+}
---
+++ OOP_Lab_5/src/Core/Algorithms/NativeFindDeterminant.cs
@@ -0,0 +1,30 @@
+﻿namespace OOP_Lab_5.Core.Algorithms
+{
+    public class NativeFindDeterminant : IFindDeterminant
+    {
+        public int Execute(Matrix matrix)
+        {
+            var det = 0;
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                var mul = 1;
+                for (int j = 0; j < matrix.Count; j++)
+                {
+                    mul *= matrix[j, (j + i) % matrix.Count];
+                }
+                det += mul;
+            }
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                var mul = 1;
+                for (int j = 0; j < matrix.Count; j++)
+                {
+                    var k = matrix.Count - i - j - 1;
+                    mul *= matrix[k >= 0 ? k : k + matrix.Count, j];
+                }
+                det -= mul;
+            }
+            return det;
+        }
+    }
+}
