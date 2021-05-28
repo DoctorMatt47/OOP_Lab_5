1113ed4ffa06b23bc90a7f8538c822be91fd53bd
DoctorMatt <DoctorMatt47@gmail.com>
2021-05-28 20:51:10 +0300

    Changed point position

--- OOP_Lab_5/src/Core/Algorithms/LibraryFindDeterminant.cs
+++ OOP_Lab_5/src/Core/Algorithms/LibraryFindDeterminant.cs
@@ -7,7 +7,8 @@ namespace OOP_Lab_5.Core.Algorithms
         public int Execute(Matrix matrix)
         {
             var matrixBuilder = Matrix<double>.Build;
-            var libMatrix = matrixBuilder.Dense(matrix.Count, matrix.Count, (i, j) => matrix[i, j]);
+            var libMatrix = matrixBuilder.
+                Dense(matrix.Count, matrix.Count, (i, j) => matrix[i, j]);
             return (int)libMatrix.Determinant();
         }
     }
