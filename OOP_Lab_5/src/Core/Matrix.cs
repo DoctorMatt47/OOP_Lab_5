f55ca6c04c043750986f1e753a73d4e60ee7addf
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-01 22:20:03 +0300

    Refreshed strategy pattern

--- OOP_Lab_5/src/Core/Matrix.cs
+++ OOP_Lab_5/src/Core/Matrix.cs
@@ -9,13 +9,10 @@ namespace OOP_Lab_5.Core
     public class Matrix : IEnumerable
     {
         private List<List<int>> _matrix;
-
         public int Count { get; }
-
         public IFindDeterminant FindDeterminantAlgorithm { private get; set; }
-
         public IFindRank FindRankAlgorithm { private get; set; }
-
+        public ITriangular TriangularAlgorithm { private get; set; }
         public IMultiply MultiplyAlgorithm { private get; set; }
 
         public Matrix(int[,] matrix)
@@ -112,21 +109,9 @@ namespace OOP_Lab_5.Core
 
         public static Matrix operator *(Matrix left, Matrix right)
         {
-            if (left.Count != right.Count)
-                throw new ArgumentException();
-
-            Matrix retMatrix = new Matrix(left.Count);
-            for (int i = 0; i < retMatrix.Count; i++)
-            {
-                for (int j = 0; j < retMatrix.Count; j++)
-                {
-                    for (int k = 0; k < retMatrix.Count; k++)
-                    {
-                        retMatrix[i, j] += left[i, k] * right[k, j];
-                    }
-                }
-            }
-            return retMatrix;
+            if (left.MultiplyAlgorithm is null)
+                left.MultiplyAlgorithm = new NativeMultiply();
+            return left.MultiplyAlgorithm.Execute(left, right);
         }
 
         public int this[int i, int j]
@@ -139,62 +124,6 @@ namespace OOP_Lab_5.Core
             {
                 _matrix[i][j] = value;
             }
-        }
-
-        private Matrix TriangularCore(out int rank)
-        {
-            rank = Count;
-
-            var matrix = new Matrix(this);
-
-            for (int row = 0; row < rank; row++)
-            {
-                if (matrix[row, row] != 0)
-                {
-                    for (int col = 0; col < Count; col++)
-                    {
-                        if (col != row)
-                        {
-                            double mult =
-                               (double)matrix[col, row] /
-                                        matrix[row, row];
-
-                            for (int i = 0; i < rank; i++)
-
-                                matrix[col, i] -= (int)mult
-                                         * matrix[row, i];
-                        }
-                    }
-                }
-                else
-                {
-                    bool reduce = true;
-
-                    for (int i = row + 1; i < Count; i++)
-                    {
-                        if (matrix[i, row] != 0)
-                        {
-                            for (int j = 0; j < rank; j++)
-                            {
-                                int temp = matrix[row, j];
-                                matrix[row, j] = matrix[i, j];
-                                matrix[i, j] = temp;
-                            }
-                            reduce = false;
-                            break;
-                        }
-                    }
-
-                    if (reduce)
-                    {
-                        rank--;
-                        for (int i = 0; i < Count; i++)
-                            matrix[i, row] = matrix[i, rank];
-                    }
-                    row--;
-                }
-            }
-            return matrix;
         }
 
         public List<List<int>> GetList() => new List<List<int>>(_matrix);
@@ -215,16 +144,24 @@ namespace OOP_Lab_5.Core
 
         public int FindDeterminant()
         {
-            if (FindDeterminantAlgorithm == null)
-                FindDeterminantAlgorithm = new MyFindDeterminant();
+            if (FindDeterminantAlgorithm is null)
+                FindDeterminantAlgorithm = new NativeFindDeterminant();
             return FindDeterminantAlgorithm.Execute(this);
         }
 
         public int FindRank()
         {
-            int rank;
-            TriangularCore(out rank);
-            return rank;
+            if (FindRankAlgorithm is null)
+                FindRankAlgorithm = new NativeFindRank();
+            return FindRankAlgorithm.Execute(this);
+        }
+
+        public Matrix Triangular()
+        {
+            if (TriangularAlgorithm is null)
+                TriangularAlgorithm = new NativeTriangular();
+
+            return TriangularAlgorithm.Execute(this);
         }
 
         public Matrix Transpose()
@@ -240,12 +177,6 @@ namespace OOP_Lab_5.Core
                 }
             }
             return retMatrix;
-        }
-
-        public Matrix Triangular()
-        {
-            int rank;
-            return TriangularCore(out rank);
         }
 
         public Matrix Square()
