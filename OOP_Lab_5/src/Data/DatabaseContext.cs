3feb15d5ad27298a67cbb452852ae0e7d6791623
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-03 17:40:49 +0300

    Added database context

---
+++ OOP_Lab_5/src/Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OOP_Lab_5.Data.Entities;
+
+namespace OOP_Lab_5.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<MatrixEntity> Matrixes { get; set; }
+
+        public DatabaseContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=OOP_Lab_5.db;Cache=Shared");
+        }
+    }
+}
---
+++ OOP_Lab_5/src/Data/Entities/MatrixEntity.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OOP_Lab_5.Core;
+using System;
+
+namespace OOP_Lab_5.Data.Entities
+{
+    public class MatrixEntity
+    {
+        public string Matrix { get; set; }
+        public int Count { get; set; }
+
+        public void Stringify(Matrix matrix)
+        {
+            Count = matrix.Count;
+            Matrix = "";
+            for (int i = 0; i < Count; i++)
+            {
+                for (int j = 0; j < Count; j++)
+                {
+                    Matrix += matrix[i, j] + ' ';
+                }
+            }
+        }
+        public Matrix ToMatrix()
+        {
+            var matrix = new Matrix(Count);
+            var matrixNumber = "";
+            int k = 0;
+            for (int i = 0; i < Count; i++)
+            {
+                for (int j = 0; j < Count; j++)
+                {
+                    while (Matrix[k] != ' ' && k < Matrix.Length)
+                    {
+                        matrixNumber += Matrix[k++];
+                    }
+                    matrix[i, j] = Convert.ToInt32(matrixNumber);
+                }
+            }
+            return matrix;
+        }
+    }
+}
