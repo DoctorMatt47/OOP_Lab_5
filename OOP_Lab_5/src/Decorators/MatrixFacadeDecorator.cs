10fff0b3429860a3fa5c2981dddd57a4005316e5
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-06 21:36:06 +0300

    Added Decorator for time measuring

---
+++ OOP_Lab_5/src/Decorators/MatrixFacadeDecorator.cs
@@ -0,0 +1,107 @@
+﻿using OOP_Lab_5.Core;
+using OOP_Lab_5.Core.Algorithms;
+using OOP_Lab_5.Core.Prototype;
+using OOP_Lab_5.Facade;
+
+namespace OOP_Lab_5.Decorators
+{
+    public class MatrixFacadeDecorator : IMatrixFacade
+    {
+        protected IMatrixFacade _facade;
+
+        public Matrix Matrix 
+        { 
+            get
+            {
+                return _facade.Matrix;
+            } 
+        }
+
+        public MatrixFacadeDecorator(IMatrixFacade facade)
+        {
+            _facade = facade;
+        }
+
+        public virtual void Transpose()
+        {
+            _facade.Transpose();
+        }
+
+        public virtual long Determinant(IFindDeterminant det)
+        {
+            return _facade.Determinant(det);
+        }
+
+        public virtual int Rank(IFindRank rank)
+        {
+            return _facade.Rank(rank);
+        }
+
+        public virtual void Square(IMultiply mul)
+        {
+            _facade.Square(mul);
+        }
+
+        public virtual void Triangular(ITriangular triangular)
+        {
+            _facade.Triangular(triangular);
+        }
+
+        public virtual void MultiplyOnScalar(long scalar)
+        {
+            _facade.MultiplyOnScalar(scalar);
+        }
+
+        public virtual void SaveToDb(string id)
+        {
+            _facade.SaveToDb(id);
+        }
+
+        public virtual void LoadFromDb(string id)
+        {
+            try
+            {
+                _facade.LoadFromDb(id);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public virtual void ChangeSize(int size)
+        {
+            _facade.ChangeSize(size);
+        }
+
+        public virtual IPrototype Copy()
+        {
+            return _facade.Copy();
+        }
+
+        public virtual void Paste(IPrototype matrix)
+        {
+            _facade.Paste(matrix);
+        }
+
+        public virtual void Undo()
+        {
+            _facade.Undo();
+        }
+
+        public virtual Matrix Add(Matrix other)
+        {
+            return _facade.Add(other);
+        }
+
+        public virtual Matrix Diff(Matrix other)
+        {
+            return _facade.Diff(other);
+        }
+
+        public virtual Matrix Multiply(Matrix other, IMultiply mul)
+        {
+            return _facade.Multiply(other, mul);
+        }
+    }
+}
---
+++ OOP_Lab_5/src/Decorators/TimeDecorator.cs
@@ -0,0 +1,164 @@
+﻿using OOP_Lab_5.Core;
+using OOP_Lab_5.Core.Algorithms;
+using OOP_Lab_5.Core.Prototype;
+using OOP_Lab_5.Facade;
+using System.Diagnostics;
+
+namespace OOP_Lab_5.Decorators
+{
+    public class TimeDecorator : MatrixFacadeDecorator
+    {
+        public long Time { get; private set; }
+
+        public TimeDecorator(IMatrixFacade facade) : base(facade) { }
+
+        public override void Transpose()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            _facade.Transpose();
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+        }
+
+        public override long Determinant(IFindDeterminant det)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var ret = _facade.Determinant(det);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+            return ret;
+        }
+
+        public override int Rank(IFindRank rank)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var ret = _facade.Rank(rank);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+            return ret;
+        }
+
+        public override void Square(IMultiply mul)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            _facade.Square(mul);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+        }
+
+        public override void Triangular(ITriangular triangular)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            _facade.Triangular(triangular);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+        }
+
+        public override void MultiplyOnScalar(long scalar)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            _facade.MultiplyOnScalar(scalar);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+        }
+
+        public override void SaveToDb(string id)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            _facade.SaveToDb(id);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+
+        }
+
+        public override void LoadFromDb(string id)
+        {
+            try
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                _facade.LoadFromDb(id);
+                stopwatch.Stop();
+                Time = stopwatch.ElapsedMilliseconds;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public override void ChangeSize(int size)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            _facade.ChangeSize(size);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+        }
+
+        public override IPrototype Copy()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var ret = _facade.Copy();
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+            return ret;
+        }
+
+        public override void Paste(IPrototype matrix)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            _facade.Paste(matrix);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+        }
+
+        public override void Undo()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            _facade.Undo();
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+        }
+
+        public override Matrix Add(Matrix other)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var ret = _facade.Add(other);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+            return ret;
+        }
+
+        public override Matrix Diff(Matrix other)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var ret = _facade.Diff(other);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+            return ret;
+        }
+
+        public override Matrix Multiply(Matrix other, IMultiply mul)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var ret = _facade.Multiply(other, mul);
+            stopwatch.Stop();
+            Time = stopwatch.ElapsedMilliseconds;
+            return ret;
+        }
+    }
+}
