0e57aa170494217948d3bc351483cf6c5cc60f4c
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-07 20:48:32 +0300

    Written documentation for IPrototype interface

--- OOP_Lab_5/src/Core/Prototype/IPrototype.cs
+++ OOP_Lab_5/src/Core/Prototype/IPrototype.cs
@@ -1,7 +1,15 @@
 ﻿namespace OOP_Lab_5.Core.Prototype
 {
+    /// <summary>
+    /// Represents interface for cloneable objects.
+    /// Implements prototype pattern.
+    /// </summary>
     public interface IPrototype
     {
+        /// <summary>
+        /// Creates clone of this object.
+        /// </summary>
+        /// <returns>clone of this object.</returns>
         IPrototype Clone();
     }
 }
